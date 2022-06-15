fa27e05e48c2f6bedf91f0b6590baa152079bd1a
W <w.yakbah@outlook.com>
2022-06-15 20:54:08 +0200

    on css : updated esthetic such as font family, text etc, however i need to compress the code. on html : removed embed. on js, initiated an email form for newsletter

---
+++ bug-enthusiasm.js
@@ -0,0 +1,6 @@
+function validateEmail(){
+    var entered= document.getElementById("email").nodeValue;
+    if (eEntered.indexOf(" ") !== -1) {4    alert("Invalid email, no spaces should be here.");
+    return false;
+    }
+}
